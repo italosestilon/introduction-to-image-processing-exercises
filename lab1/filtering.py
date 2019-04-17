d005d5a128ac664581a30d9b0d8cce980c94164b
Italos Estilon de Souza <italosestilon@gmail.om>
2019-04-17 11:19:16 -0300

    fix filters

--- lab1/filtering.py
+++ lab1/filtering.py
@@ -1,8 +1,10 @@
 import numpy as np
 from scipy import ndimage
-from skimage import io, img_as_ubyte, img_as_float64, img_as_uint
+from skimage import io, img_as_ubyte, img_as_float, img_as_int
 from skimage import transform
 from scipy.fftpack import ifft2, fft2, fftshift
+
+from skimage.filters import sobel_h, sobel_v
 
 import cv2
 
@@ -14,7 +16,7 @@ args = parser.parse_args()
 
 def load_image(dir):
     image = io.imread(dir)
-    return image
+    return img_as_int(image)
 
 def filter(image, kernel):
     image_filtered = ndimage.convolve(image, kernel, mode='constant', cval=0)
@@ -34,23 +36,33 @@ high = np.array([[0,-1, 0], [-1, 4, -1], [0, -1, 0]])
 
 image = load_image(args.image_dir)
 
+image_filtered_h3 = filter(image, h3)
 image_filtered_h2 = filter(image, h2)
-image_filtered_h1 = filter(image, h1)
-image_filtered_h3 = filter(image_filtered_h2, h3)
-image_filtered_h4 = filter(image_filtered_h2, h4)
-filtered_h3_h4 = filter(image_filtered_h2, np.sqrt(h3**2 + h4**2))
+image_filtered_h1 = filter(image, high)
+#h1_after_h2 = filter(image_filtered_h2, h1)
+image_filtered_h4 = filter(image, h4)
+filtered_h3_h4 = np.sqrt(img_as_float(image_filtered_h3)**2 + img_as_float(image_filtered_h4)**2)
+
+print(filtered_h3_h4.max())
+#print('shape', image.shape)
 
 #print(image_filtered_h2.max())
 #print(np.sqrt(image_filtered_h3**2 + image_filtered_h4**2))
 
+#f = np.absolute(image_filtered_h3)
+#print()
+f = filtered_h3_h4
+f = 1*(f - f.min())/(f.max() - f.min())
+
 io.imsave('filtered_h1.png', image_filtered_h1)
-io.imsave('filtered_h2.png', image_filtered_h2)
+io.imsave('filtered_h2.png', image_filtered_h2) 
+#io.imsave('h1_after_h2.png', h1_after_h2)
 io.imsave('filtered_h3.png', image_filtered_h3)
 io.imsave('filtered_h4.png', image_filtered_h4)
-io.imsave('filtered_h3_h4.png', filtered_h3_h4)
+io.imsave('filtered_h3_h4.png', f)
 
 fft_image = discrete_fourier_transform(image)
 
-print(np.real(fft_image))
+#print(np.real(fft_image))
 
-io.imsave('fft_image.png', (np.real(fft_image)/np.real(fft_image).max()))
+#io.imsave('fft_image.png', (np.real(fft_image)/np.real(fft_image).max()))
